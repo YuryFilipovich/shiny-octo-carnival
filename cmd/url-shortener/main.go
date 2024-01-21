6fa7960a0c1020f3622c90c5535ffeaf8815610f
YURY FILIPOVICH <y_filipovich@icloud.com>
2024-01-21 22:19:41 +0100

    Updated main.go

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,5 @@
 package main
 
 func main() {
-	//	TODO: init config: cleanenv
 
-	//	TODO: init logger: slog
-
-	//	 TODO: init storage: sqlite
-
-	//	TODO: init router: chi, render
-
-	//	TODO: run server:
 }
